d014e7086fa6f1b751109338e88fdff608a1962e
Zachary Moore <zacharymichaelmoore@gmail.com>
2016-09-24 14:10:17 -0700

    Add conversation data

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,4 +1,13 @@
 'use strict'
+const firstOfEntityRole = function(message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots
+  const entityValues = message.slots[entity]
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+
+  return valsForRole ? valsForRole[0] : null
+}
 
 exports.handle = function handle(client) {
 
@@ -29,17 +38,51 @@ exports.handle = function handle(client) {
     }
   })
 
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
+
+    extractInfo() {
+      const city = firstOfEntityRole(client.getMessagePart(), 'city')
+
+      if (city) {
+        client.updateConversationState({
+          weatherCity: city,
+        })
+
+        console.log('User wants the weather in:', city.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:promt/weather_city')
+      client.done()
+    },
+  }
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      // Need to provide weather
+      client.done()
+    },
+  })
+
   client.runFlow({
     classifications: {
 			// map inbound message classifications to names of streams
     },
-    autoResponses: {
+    //autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
-    },
+    //},
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained]
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
     }
   })
 }
